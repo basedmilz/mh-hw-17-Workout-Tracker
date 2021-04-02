f02eff7731af5596ff1413909912be9a4fc35c2f
basedmilz <basedmilz@gmail.com>
2021-04-02 16:18:51 -0400

    added to workout model

--- models/workout.js
+++ models/workout.js
@@ -3,40 +3,53 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    date: {
-type: Date,
-default: Date.now
-    },
+  day: {
+    type: Date,
+    default: Date.now
+  },
 
-    exercises: 
+  exercises: [
     {
-        type: {
-            type: String,
-            trim: true,
-            required: "Type of exercise"
-        },
+      type: {
+        type: String,
+        trim: true,
+        required: "Type of exercise?"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Name of exercise?"
+      },
 
+      duration: {
+        type: Number,
+        required: "How long was your exercise?"
+      },
+
+      weight: {
+        type: Number,
         
-    }
+      },
+
+      reps: {
+        type: Number,
+        
+
+      },
+
+      sets: {
+        type: Number,
+        
+      },
+
+      distance: {
+        type: Number,
+      }
+    }]
 
 })
 
-// --------------
-const transactionSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-});
+
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
